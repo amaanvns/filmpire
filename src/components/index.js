7d1f59a6ca92b40458a4d73a3ee3969c9973d77f
amaanvns <amaanarshad007@gmail.com>
2022-11-23 18:48:21 +0530

    files and folder structure

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Actors} from './Actors/Actors'
+export {default as MoviesInformation} from './MovieInformation/MoviesInformation'
+export {default as Movies} from './Movies/Movies'
+export {default as Profile} from './Profile/Profile'
+export {default as NavBar} from './NavBar/NavBar'
+
